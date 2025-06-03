896464c2b13ed63ea36fe048d164fc11c1c85940
Juan Diego Delgado Arciniega <jddelgado5@espe.edu.ec>
2025-06-03 12:28:24 -0500

    Delete ws/delgado/ws02HtmlAndPhp directory

--- ws/delgado/ws02HtmlAndPhp/php/conection.php
+++
@@ -1,9 +0,0 @@
-<?php
-$host = 'localhost';
-$dbname = 'computers';
-$username = 'root';
-$password = 'rootroot';
-
-$pdo = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
-$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_SILENT);
-?>
