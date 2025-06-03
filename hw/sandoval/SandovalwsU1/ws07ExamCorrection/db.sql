d26b5a46347a2f7a29822a046937b261f0c6be3c
Rodrigo Fernando Sandoval Lara <132413789+rfsandoval1@users.noreply.github.com>
2025-06-02 20:06:58 -0500

    Delete hw/sandoval/SandovalwsU1 directory

--- hw/sandoval/SandovalwsU1/ws07ExamCorrection/db.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE songs (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    title VARCHAR(100) NOT NULL,
-    artist VARCHAR(100) NOT NULL,
-    duration INT NOT NULL, -- duración en segundos
-    genre VARCHAR(50),
-    release_date DATE
-);
